223970314d644c714af0d34209fa4515027ae1cc
dolphin <dolphin>
2017-04-25 19:37:52 +0800

    some

--- public/pay.php
+++ public/pay.php
@@ -2,30 +2,11 @@
 
 var_dump("Pay");
 
-// require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-function https_post($url, $data = null)
-{
-    $curl = curl_init();
+$json    = array();
 
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
-
-    if( ! empty($data) ) 
-    {
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-    }
-
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-    $output = curl_exec($curl);
-
-    curl_close($curl);
-    
-    return $output;
-}
+Requests::register_autoloader();
 
 $server     = "https://api.mch.weixin.qq.com/pay/unifiedorder";
 $randstr    = 'sdfew8j2f0g938fk5de825ddfgr2sxz6';
@@ -38,7 +19,7 @@ $request = array(
     'mch_id' => '1460504502',
     'device_info' => 'WEB',
     'nonce_str' => $randstr,
-    'body' => '金宁文化旅游股份有限公司-支付测试',
+    'body' => '支付测试',
     'out_trade_no' => $orderNo,
     'total_fee' => 1,
     'spbill_create_ip' => $ip_address,
@@ -62,15 +43,15 @@ $sign = strtoupper(md5($strTemp));
 var_dump($sign);
 
 $xml = "<xml>
-<appid>wxeb58e2715cd8a221</appid>
-<mch_id>1276370801</mch_id>
+<appid>wx3f57772b43b05ba5</appid>
+<mch_id>1460504502</mch_id>
 <device_info>WEB</device_info>
 <nonce_str>{$randstr}</nonce_str>
-<body>南京商法通法律咨询服务有限公司-支付测试</body>
+<body>支付测试</body>
 <out_trade_no>{$orderNo}</out_trade_no>
 <total_fee>1</total_fee>
 <spbill_create_ip>{$ip_address}</spbill_create_ip>
-<notify_url>http://www.blb.com.cn/pay/callback.php</notify_url>
+<notify_url>http://mobie.hbdx.cc/</notify_url>
 <trade_type>JSAPI</trade_type>
 <openid>{$openid}</openid>
 <sign>{$sign}</sign>
@@ -78,6 +59,46 @@ $xml = "<xml>
 
 var_dump($xml);
 
-$res    = https_post($server, $xml);
+$request = Requests::post($server, array(), $xml);
 
-var_dump($res);
+if ($request->status_code != 200) {
+    $json['code'] = 2;
+    $json['msg']  = '请求预支付订单失败';
+
+    echo json_encode($json);
+}
+
+$reader = new Sabre\Xml\Reader();
+$reader->xml($request->body);
+$result = $reader->parse();
+
+$prepay = "prepay_id=";
+
+foreach ($result['value'] as $key => $value) {
+    if ($value['name'] == '{}prepay_id') {
+        $prepay .= $value['value'];
+    }
+}
+
+$data = array(
+    'appId' => 'wx3f57772b43b05ba5',
+    'timeStamp' => time(),
+    'nonceStr' => $randstr,
+    'package' => $prepay,
+    'signType' => 'MD5');
+
+ksort($data);
+
+$str2 = urldecode(http_build_query($data));
+
+$strTemp = $str2 . "&key=" . $key;
+
+$sign2 = strtoupper(md5($strTemp));
+
+$data['paySign'] = $sign2;
+
+$json['code'] = 0;
+$json['msg']  = '请求预支付交易单成功';
+$json['data'] = $data;
+
+echo json_encode($json);
