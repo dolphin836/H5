fe2898e6f2fe5218b3357e946b008313572eaa9a
dolphin <dolphin>
2017-04-27 10:13:04 +0800

    some

--- controller/ticket.php
+++ controller/ticket.php
@@ -13,9 +13,32 @@ class Ticket extends Controller
 
     public function index()
     {
+        $results        = $this->app->db->select('ticket', ['id', 'code', 'product_name', 'product_price', 'status', 'create_time'], ['uuid[=]' => $_SESSION['uuid']]);
+        $ticket_open    = array();
+        $ticket_close   = array();
+        foreach ($results as $result) {
+            if ($result['status'] == 0) {
+                $ticket_open[]  = array(
+                        'code'  => $result['code'],
+                 'product_name' => $result['product_name'],
+                'product_price' => $result['product_price'],
+                       'status' => $result['status'],
+                  'create_time' => date("Y-m-d H:i:s", $result['create_time'])
+                );
+            } else {
+               $ticket_close[]  = array(
+                        'code'  => $result['code'],
+                 'product_name' => $result['product_name'],
+                'product_price' => $result['product_price'],
+                       'status' => $result['status'],
+                  'create_time' => date("Y-m-d H:i:s", $result['create_time'])
+                );
+            }
+        }
+
         $scripts[] = $this->server . 'dist/js/' . 'zepto.min.js';
         $scripts[] = $this->server . 'dist/js/' . 'ticket.js';
 
-        echo $this->app->template->render('ticket', ['server' => $this->server, 'item' => 'ticket', 'cartCount' => $this->cartCount, 'scripts' => $scripts]);
+        echo $this->app->template->render('ticket', ['server' => $this->server, 'item' => 'ticket', 'cartCount' => $this->cartCount, 'scripts' => $scripts, 'ticket_open' => $ticket_open, 'ticket_close' => $ticket_close]);
     }
 }
