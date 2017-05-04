e2911f2586d6068a4eaa2663690b375a66fe3cff
dolphin <dolphin>
2017-05-04 14:35:34 +0800

    some

--- public/zhi.php
+++ public/zhi.php
@@ -2,7 +2,7 @@
 
 var_dump($_SERVER);
 
-if (strpos($_SERVER['HTTP_USER_AGENT'], 'AlipayClient') !== false && $_SERVER['QUERY_STRING'] != '' ) { // 支付宝浏览器
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'AlipayClient') !== false && $_SERVER['QUERY_STRING'] == '' ) { // 支付宝浏览器
         $back    = urlencode("http://m.outatv.com/zhi.php");
 
         $uri     = "https://openauth.alipay.com/oauth2/publicAppAuthorize.htm?app_id=2017050207083850&scope=auth_userinfo&redirect_uri=" . $back;
