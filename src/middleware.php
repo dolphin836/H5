717545545cac6ce944e8d4beb4e617465de8fa6f
dolphin <416509859@qq.com>
2017-05-09 18:13:29 +0800

    some

--- src/middleware.php
+++ src/middleware.php
@@ -62,7 +62,7 @@ $app->add(function ($request, $response, $next) {
                 $sign         = $this->tool->sign($data);
                 $data['sign'] = $sign;
 
-                $this->logger->addInfo($data);
+                $this->logger->addInfo("data:", $data);
 
                 Requests::register_autoloader();
                 $response = Requests::post($zhi, array(), $data);
